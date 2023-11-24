76043bd1b23a306ed45650503ad6c47062e99ef9
SebEspinoza <seba.espinozal@duocuc.cl>
2023-11-24 17:43:23 -0300

    changed prediction screen

--- src/pages/Estimaciones.jsx
+++ src/pages/Estimaciones.jsx
@@ -7,9 +7,8 @@ const Estimaciones = () => {
       <h1 className="text-5xl underline text-center text-color-cafe-claro mt-2">Estimaciones</h1>
       <p className="text-center mt-4 text-lg text-color-cafe-claro md:mb-4">
         En este apartado podrás ocupar el modelo de predicción de stock, para poder predecir el stock necesario dentro de un mes
-        La cantidad y mes deben ser valores númericos. Ejemplo: Cantidad: 100, Mes: 1 (Enero)
       </p>
-      <Iframe src="https://knn-weatheraus.onrender.com/" height="52%" width="100%" scrolling="no" />
+      <Iframe src="https://knn-weatheraus.onrender.com/" height="89%" width="100%" scrolling="no" />
     </div>
   );
 };
