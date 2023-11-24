57f9213fc4a558bb1dbdd22224bbdc5b653e1362
SebEspinoza <seba.espinozal@duocuc.cl>
2023-11-24 10:06:21 -0300

    added basic prediction page

--- src/pages/Estimaciones.jsx
+++ src/pages/Estimaciones.jsx
@@ -1,8 +1,17 @@
-import { useParams } from "react-router-dom";
+import React from "react";
+import Iframe from "react-iframe";
 
 const Estimaciones = () => {
-  const { aID } = useParams();
-  return <h1>Estimaciones / {aID}</h1>;
+  return (
+    <div className="h-screen">
+      <h1 className="text-5xl underline text-center text-color-cafe-claro mt-2">Estimaciones</h1>
+      <p className="text-center mt-4 text-lg text-color-cafe-claro md:mb-4">
+        En este apartado podrás ocupar el modelo de predicción de stock, para poder predecir el stock necesario dentro de un mes
+        La cantidad y mes deben ser valores númericos. Ejemplo: Cantidad: 100, Mes: 1 (Enero)
+      </p>
+      <Iframe src="https://knn-weatheraus.onrender.com/" height="52%" width="100%" scrolling="no" />
+    </div>
+  );
 };
 
 export default Estimaciones;
