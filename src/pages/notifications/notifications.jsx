f4ac01c3d4df9cce440fdfe2ed8a026b0790b03d
SebEspinoza <seba.espinozal@duocuc.cl>
2023-11-27 21:50:49 -0300

    changed title

--- src/pages/notifications/notifications.jsx
+++ src/pages/notifications/notifications.jsx
@@ -4,7 +4,7 @@ import NotifList from './notifList';
 
 const Notifications = () => {
     return (
-        <><h1 className="text-5xl underline text-center text-color-cafe-claro mt-2">Alertas</h1>
+        <>
             <p className="text-center mt-4 text-lg text-color-cafe-claro md:mb-4">
                 En este apartado podrás ver los productos que tienen un stock menor a 15 unidades, que se encuentran vencidos o pronto a vencer.
             </p>
