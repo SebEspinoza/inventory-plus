6f6a3fe14e580077b863d4f5ae21a2caa72eb754
SebEspinoza <seba.espinozal@duocuc.cl>
2023-12-03 19:12:12 -0300

    responsive changes

--- src/pages/predictions/Estimaciones.jsx
+++ src/pages/predictions/Estimaciones.jsx
@@ -4,14 +4,14 @@ import EstiList from "./estiList";
 
 const Estimaciones = () => {
   return (
-    <div className="h-screen flex-1 flex flex-wrap justify-center">
-      <p className="text-center mt-4 text-sm text-color-cafe-claro md:mb-4">
-        En este apartado podrás ocupar el modelo de predicción de stock, para poder predecir el stock necesario dentro de un mes
-      </p>
-      <div className="w-full md:w-1/2">
-        <Iframe src="https://knn-weatheraus.onrender.com/" height="89%" width="100%" scrolling="no" />
+    <div className="flex flex-col md:flex-row">
+      <div className="md:w-1/2">
+        <p className="text-center mt-4 text-sm text-color-cafe-claro md:mb-4">
+          En este apartado podrás ocupar el modelo de predicción de stock, para poder predecir el stock necesario dentro de un mes
+        </p>
+        <Iframe src="https://knn-weatheraus.onrender.com/" height="400px" width="100%" scrolling="no" />
       </div>
-      <div className="w-full md:w-1/2">
+      <div className="md:w-1/2">
         <EstiList />
       </div>
     </div>
